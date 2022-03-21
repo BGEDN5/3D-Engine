f0d0298a6f3d10166802b023f9a2888684be1cc1
vlasi-p <vlasivlasi123@gmail.com>
2022-03-22 01:34:56 +0400

    Update .gitignore

---
+++ src/main/java/Matrix4f.java
@@ -0,0 +1,53 @@
+public class Matrix4f {
+
+    private static final short MATRIX_LEN = 4;
+    private float[][] matrix;
+
+    public Matrix4f(float [][] matrix){
+        this.matrix = matrix;
+    }
+
+    public float[][] getMatrix() {
+        return matrix;
+    }
+
+    public float getCellValue(int i, int j){
+        return matrix[i][j];
+    }
+
+    public void setMatrix(float[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public void setSeparateCell(int i, int j, float entry){
+        this.matrix[i][j] = entry;
+    }
+
+    public static Matrix4f identity(){
+        float[][] id_mat = new float[MATRIX_LEN][MATRIX_LEN];
+
+        for (int i = 0; i < MATRIX_LEN; i++) {
+            for (int j = 0; j < MATRIX_LEN; j++) {
+                id_mat[i][j] = (i == j) ? 1 : 0;
+            }
+        }
+        return new Matrix4f(id_mat);
+    }
+
+    public void multiply(Matrix4f other){
+
+        float[][] matrix1 = new float[MATRIX_LEN][MATRIX_LEN];
+
+        for(int i = 0; i< MATRIX_LEN; i++) {
+            for (int j = 0; j < MATRIX_LEN; j++) {
+                matrix1[i][j] = 0;
+                for (int k = 0; k < MATRIX_LEN; k++) {
+                    matrix1[i][j] += getMatrix()[i][k] * other.getMatrix()[k][j];
+                }
+            }
+        }
+        setMatrix(matrix1);
+    }
+
+
+}
