e72a6cafb212f4f37193fa4144110272f3a8be0d
TomaPirtskhelani <Pirtskhelani.Toma@kiu.edu.ge>
2022-04-04 09:06:10 +0400

    Add Vertex Class

---
+++ src/main/java/Vertex.java
@@ -0,0 +1,21 @@
+public class Vertex {
+
+   private Vector3f pos;
+
+   public Vertex(){
+       pos = new Vector3f(0F, 0F, 0F);
+   }
+
+   public Vertex(float x1, float y1, float z1){
+        pos = new Vector3f(x1, y1, z1);
+   }
+
+   public void set(Vector3f x){
+       this.pos = x;
+   }
+
+   public Vector3f get(){
+       return this.pos;
+   }
+
+}
