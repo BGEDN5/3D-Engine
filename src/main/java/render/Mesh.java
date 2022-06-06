17726ecbdb0f28fd3ad0f39cb02f98d119b3f064
DavitMamrikishvili <davit.mamrikishvili1@gmail.com>
2022-06-06 23:22:10 +0400

    Update Mesh with Mesh Face

--- src/main/java/render/Mesh.java
+++ src/main/java/render/Mesh.java
@@ -2,46 +2,57 @@ package render;
 
 import maths.Matrix4f;
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL20;
+
+import static org.lwjgl.opengl.GL15.glGenBuffers;
+import static org.lwjgl.opengl.GL20.*;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 
 public class Mesh {
-    private int vbo; // vertex buffer object
+    private final int vbo; // vertex buffer object
+    private final int ibo; // index buffer object
     private int size;
 
     public Mesh() {
-        this.vbo = GL20.glGenBuffers();
+        this.vbo = glGenBuffers();
+        this.ibo = glGenBuffers();
         this.size = 0;
     }
 
-    public Mesh(Vertex[] vertices) {
-        this.vbo = GL20.glGenBuffers();
+    public Mesh(Vertex[] vertices, int[] indices) {
+        this.vbo = glGenBuffers();
+        this.ibo = glGenBuffers();
         this.size = 0;
-        addVertices(vertices);
+        addVertices(vertices, indices);
     }
 
-    public void addVertices(Vertex[] vertices) {
-        this.size += vertices.length;
-        GL20.glBindBuffer(GL20.GL_ARRAY_BUFFER, this.vbo);
-        GL20.glBufferData(GL20.GL_ARRAY_BUFFER, createFlippedBuffer(vertices), GL20.GL_STATIC_DRAW);
+    public void addVertices(Vertex[] vertices, int[] indices) {
+        this.size += indices.length;
+
+        glBindBuffer(GL_ARRAY_BUFFER, this.vbo);
+        glBufferData(GL_ARRAY_BUFFER, createFlippedBuffer(vertices), GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this.ibo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, createFlippedBuffer(indices), GL_STATIC_DRAW);
     }
 
     public void draw() {
-        GL20.glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(0);
 
-        GL20.glBindBuffer(GL20.GL_ARRAY_BUFFER, vbo);
-        GL20.glVertexAttribPointer(0, 3, GL20.GL_FLOAT, false, Vertex.SIZE * 4, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, Vertex.SIZE * 4, 0);
 
-        GL20.glDrawArrays(GL20.GL_TRIANGLES, 0, size);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        glDrawElements(GL_TRIANGLES, this.size, GL_UNSIGNED_INT, 0);
 
-        GL20.glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(0);
     }
 
     public static FloatBuffer createFlippedBuffer(Vertex[] vertices) {
         FloatBuffer buffer = BufferUtils.createFloatBuffer(vertices.length * Vertex.SIZE);
 
-        for(int i = 0; i < vertices.length; i++) {
+        for (int i = 0; i < vertices.length; i++) {
             buffer.put(vertices[i].get().getX());
             buffer.put(vertices[i].get().getY());
             buffer.put(vertices[i].get().getZ());
@@ -64,8 +75,19 @@ public class Mesh {
         return buffer;
     }
 
+    public static IntBuffer createFlippedBuffer(int[] indices) {
+        IntBuffer buffer = BufferUtils.createIntBuffer(indices.length);
+        buffer.put(indices);
+        buffer.flip();
+        return buffer;
+    }
+
     public int getVbo() {
         return vbo;
+    }
+
+    public int getIbo() {
+        return ibo;
     }
 
     public int getSize() {
