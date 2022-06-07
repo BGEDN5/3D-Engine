2a29f547d89fc56d742d68e9e6e7164753b0bcde
DavitMamrikishvili <davit.mamrikishvili1@gmail.com>
2022-06-07 10:19:38 +0400

    Fix MeshLoader

--- src/main/java/render/MeshLoaderObj.java
+++ src/main/java/render/MeshLoaderObj.java
@@ -12,12 +12,13 @@ public class MeshLoaderObj implements MeshLoader {
 
     @Override
     public Mesh loadMesh(String path) throws IOException {
-        Vertex[] vertices = new Vertex[0];
+        Vertex[] vertices;
         List<Integer> list = new ArrayList<>();
         Stream<String[]> stream = Files.lines(Path.of(path)).map(line -> line.split(" "));
-        vertices = stream.filter(line -> line[0].equals("v"))
+        vertices = (Vertex[]) stream.filter(line -> line[0].equals("v"))
                 .map(line -> new Vertex(Float.parseFloat(line[1]), Float.parseFloat(line[2]), Float.parseFloat(line[3])))
-                .toList().toArray(vertices);
+                .toArray();
+
         stream.filter(line -> line[0].equals("f"))
                 .forEach(line -> list.addAll(List.of(Integer.parseInt(line[1]), Integer.parseInt(line[2]), Integer.parseInt(line[3]))));
         int[] indices = new int[list.size()];
@@ -25,6 +26,11 @@ public class MeshLoaderObj implements MeshLoader {
             indices[i] = list.get(i);
         }
         return new Mesh(vertices, indices);
+    }
+
+    public static void main(String[] args) throws IOException {
+        MeshLoader loader = new MeshLoaderObj();
+        loader.loadMesh("src/main/resources/meshes/cube.obj");
     }
 
 }
